076eafffb549ba62d4a94dd5c6cc1354db966656
Justus <tinnjust@kkg-do.eu>
2024-09-25 13:14:01 +0200

    df

--- src/MainControl.java
+++ src/MainControl.java
@@ -1,22 +1,26 @@
-public class MainControl {
+    public class MainControl {
 
-    public static void main(String[] args) {
-        new Person();
-        new Person();
-        new Person();
-        new Person();
-        //new Fahrzeug();
-        halloWelt();
+        //Attribute
+        private Person person1;
+        private Person person2, person3;
+
+        //Konstruktor
+        public static void main(String[] args) {
+            //halloWelt();
+            new MainControl();
+
+        }
+
+        public MainControl() {
+            person1 = new Person("Peter", 17);
+            person2 = new Person("Sabine", 15);
+
+            person1.halloSagen();
+            person2.halloSagen();
+
+
+            System.out.println(person1);
+
+            System.out.println(person2);
+        }
     }
-    private static void halloWelt(){
-        System.out.println("Hello World!");
-        //System.out.print("Hello World!");
-        //System.out.println("Hello World!");
-        //System.out.print("Hello World!");
-        //System.out.println("Hello World!");
-        //System.out.print("Hello World!");
-
-    }
-
-}
-
--- src/Person.java
+++ src/Person.java
@@ -1,18 +1,19 @@
 public class Person {
 
-    //Atribute
-    private String name = "Peter";
-    private int age = 15;
+    //Attribute
+    private String name;
+    private int age;
 
     //Referenzen
 
-    // Konstruktor
-    public Person(){
-        hallosagen();
-    }
+    //Konstruktor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
 
+    }
     //Methoden
-    public void hallosagen(){
+    public void halloSagen(){
         System.out.println("Well hello there, are you a ginger?");
         System.out.println("Oh? You are! Then you have been suffering for " + age + " years.") ;
         System.out.println("So go to finish hospital KYS to get cured " + name + ";)");
